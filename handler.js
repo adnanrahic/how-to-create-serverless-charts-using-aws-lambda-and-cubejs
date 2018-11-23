e9c9aab3913a247d7fc0a5d0f145559cd068a69c
adnanrahic <ado.raha198@gmail.com>
2018-11-23 17:11:23 +0100

    fix

--- handler.js
+++ handler.js
@@ -3,8 +3,11 @@
 const { CanvasRenderService } = require('chartjs-node-canvas')
 const fs = require('fs')
 const { promisify } = require('util')
+const cubejs = require('@cubejs-client/core')
+
 const readFileAsync = promisify(fs.readFile)
 const writeFileAsync = promisify(fs.writeFile)
+const cubejsApi = cubejs(process.env.CUBEJS_TOKEN)
 
 // bug workaround: https://github.com/vmpowerio/chartjs-node/issues/26
 if (global.CanvasGradient === undefined) {
@@ -12,45 +15,15 @@ if (global.CanvasGradient === undefined) {
 }
 
 async function renderChartWithChartJS () {
+  const resultSet = await cubejsApi.load({
+    measures: ['Container.count'],
+    dimensions: ['Container.type']
+  })
+  console.log(resultSet)
+
   const width = 400
   const height = 400
-  const configuration = {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            callback: (value) => '$' + value
-          }
-        }]
-      }
-    }
-  }
+
   const chartCallback = (ChartJS) => {
     ChartJS.defaults.global.responsive = true
     ChartJS.defaults.global.maintainAspectRatio = false
@@ -58,7 +31,7 @@ async function renderChartWithChartJS () {
 
   console.log('Rendering chart')
   const canvasRenderService = new CanvasRenderService(width, height, chartCallback)
-  return canvasRenderService.renderToBuffer(configuration)
+  return canvasRenderService.renderToBuffer(resultSet)
 }
 
 module.exports.generate = async (event) => {
@@ -70,6 +43,15 @@ module.exports.generate = async (event) => {
   const response = {
     statusCode: 200,
     body: null
+  }
+
+  return response
+}
+
+module.exports.hello = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'hello' })
   }
 
   return response
